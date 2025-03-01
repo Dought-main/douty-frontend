94618929b3474e02fffcd18a595e03bea64e6960
Oshan <oshankhan07@gmail.com>
2025-03-01 19:37:43 +0530

    dash board

--- src/app/(dashboard)/components/JourneySection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const JourneySection = () => {
-    return (
-        <div>  </div>
-    )
-}
-
-export default JourneySection
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from './(dashboard)/components/HeroSection'
 import SubHeroSection from './(dashboard)/components/SubHeroSection'
+import SubSection3 from './(dashboard)/components/SubSection3'
 
 const page = () => {
   return (
     <div>
       <HeroSection />
       <SubHeroSection />
+      <SubSection3/>
     </div>
   )
 }
